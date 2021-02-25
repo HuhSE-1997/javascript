d0d22e2d3f5db31a8406e0bdbd21e7631368fb3c
HuhSE-1997 <gjgjtprkd@gmail.com>
2021-02-25 22:21:57 +0900

    const,let

--- Practice.js
+++ Practice.js
@@ -1,1 +1,11 @@
-console.log("dkfsokfopds")
+// conts let
+
+// const 변하지않는 값, 값을 다시 정의 할수 없다. 
+const person = "영희";
+// let 값을 바꿀 수 있다.
+let people = "철수";
+
+people = "철수 엄마";
+
+console.log(people);
+console.log(person);
