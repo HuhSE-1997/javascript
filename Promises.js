d557e6acb5aa6c6d72f6238f0b9b167e7106a1b1
HuhSE-1997 <gjgjtprkd@gmail.com>
2021-02-27 21:02:47 +0900

    Promises

--- Promises.js
+++ Promises.js
@@ -42,4 +42,74 @@ const AmiSexy = new Promise((resolve, reject) => {
         .then((lastNumber) => console.log(lastNumber))
         .catch((error) => console.log(error));
       
+//   ************************************Promise.all()**************************************************
+const p1 = new Promise(resolve =>{
+    setTimeout(resolve, 5000, "gimochi")
+  })
+  
+  const p2 = new Promise(resolve =>{
+    setTimeout(resolve, 3000, "gimochi2")
+  })
+  
+  const p3 = new Promise(resolve =>{
+    setTimeout(resolve, 1000, "gimochi3")
+  })
+  
+  // p1,p2,p3, these thiings put together in array
+  const motherPromise = Promise.all([p1,p2,p3]);
+  
+  motherPromise.then(values => console.log(values))
+//   result: ["gimochi","gimochi2","gimochi3"]
+
+//   ************************************Promise.all() if they have reject**************************************************
+
+const p1 = new Promise(resolve =>{
+    setTimeout(resolve, 5000, "gimochi")
+  })
+  
+  const p2 = new Promise((resolve, reject) =>{
+    setTimeout(reject, 3000, "i hate everything ")
+  })
+  
+  const p3 = new Promise(resolve =>{
+    setTimeout(resolve, 1000, "gimochi3")
+  })
+  
+  
+  const motherPromise = Promise.all([p1,p2,p3]);
+  
+  motherPromise.then(values => console.log(values)).catch(err => console.log(err))
+
+//   result: i hate everything
+
+
+//   ************************************Promise.race()**************************************************
+// Promise.race()는 promise 3개중 가장 빠른 resolve 나 reject 출력 해준다.
+
+const p1 = new Promise(resolve =>{
+    setTimeout(resolve, 5000, "gimochi")
+  })
+  
+  const p2 = new Promise((resolve, reject) =>{
+    setTimeout(reject, 3000, "i hate everything ")
+  })
+  
+  const p3 = new Promise(resolve =>{
+    setTimeout(resolve, 1000, "gimochi3")
+  })
+  
+  
+Promise.race([p1,p2,p3]).then(values => console.log(values)).catch(err => console.log(err));
+
+// result= gimochi3
+
+//   ************************************.finally**************************************************
+// resolve or reject 둘중 하나 되면 finally 나중에 같이 나옴
+
+const p1 = new Promise((resolve,reject) => {
+    setTimeout(reject, 5000, "gimochi");
+  })
+    .then((value) => console.log(value))
+    .catch((err)=> console.log(`${err} go`))
+    .finally(() => console.log("good"));
   
